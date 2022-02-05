139dcc097dec404b6f36e0c00cc69727a3bb1332
Gabriel Seabra <gabrielpcgs81@gmail.com>
2022-02-05 12:42:26 -0300

    operacoes basicas

--- script.js
+++ script.js
@@ -6,11 +6,126 @@ wrapper.addEventListener('click', (event) => {
         return;
     }
 
-    //console.dir(event.target.innerHTML);
-    display(event.target.innerHTML);
+    var key = event.target.className;
+
+    switch (key) {
+        case 'number':
+            console.log(key);
+            display_atual(event.target.textContent);
+            break;
+        case 'dot':
+            console.log(key);
+            display_atual(event.target.textContent);
+            break;
+        case 'clear':
+            clear_atual();
+            clear_ultimo();
+            console.log(key);
+            break;
+        case 'operation sum':
+            add(event.target.textContent);
+            break;
+        case 'operation sub':
+            subtract(event.target.textContent);
+            break;
+        case 'operation mult':
+            multiply(event.target.textContent);
+            break;
+        case 'operation div':
+            divide(event.target.textContent);
+            break;
+        //clear();
+    }
 })
 
-function display(text) {
-    const visor = document.getElementById('visor');
-    visor.innerHTML = visor.innerHTML + text;
+function display_atual(text) {
+    const visor = document.getElementById('atual');
+    visor.textContent = visor.textContent + text;
+}
+
+function display_ultimo(text) {
+    const visor = document.getElementById('ultimo');
+    visor.textContent = visor.textContent + text;
+}
+
+function clear_atual() {
+    const visor = document.getElementById('atual');
+    visor.textContent = '';
+}
+
+function clear_ultimo() {
+    const visor = document.getElementById('ultimo');
+    visor.textContent = '';
+}
+
+function add(text) {
+    const ultimo = document.getElementById('ultimo').textContent;
+    if (ultimo == '') {
+        const atual = document.getElementById('atual').textContent;
+        display_ultimo(atual);
+        clear_atual();
+    }
+    else {
+        const atual = document.getElementById('atual').textContent;
+        a = parseInt(atual, 10);
+        u = parseInt(ultimo, 10);
+        sum = a + u;
+        clear_ultimo();
+        display_ultimo(sum);
+        clear_atual();
+    }
+}
+
+function subtract(text) {
+    const ultimo = document.getElementById('ultimo').textContent;
+    if (ultimo == '') {
+        const atual = document.getElementById('atual').textContent;
+        display_ultimo(atual);
+        clear_atual();
+    }
+    else {
+        const atual = document.getElementById('atual').textContent;
+        a = parseInt(atual, 10);
+        u = parseInt(ultimo, 10);
+        sum = u - a;
+        clear_ultimo();
+        display_ultimo(sum);
+        clear_atual();
+    }
+}
+
+function multiply(text) {
+    const ultimo = document.getElementById('ultimo').textContent;
+    if (ultimo == '') {
+        const atual = document.getElementById('atual').textContent;
+        display_ultimo(atual);
+        clear_atual();
+    }
+    else {
+        const atual = document.getElementById('atual').textContent;
+        a = parseInt(atual, 10);
+        u = parseInt(ultimo, 10);
+        sum = a * u;
+        clear_ultimo();
+        display_ultimo(sum);
+        clear_atual();
+    }
+}
+
+function divide(text) {
+    const ultimo = document.getElementById('ultimo').textContent;
+    if (ultimo == '') {
+        const atual = document.getElementById('atual').textContent;
+        display_ultimo(atual);
+        clear_atual();
+    }
+    else {
+        const atual = document.getElementById('atual').textContent;
+        a = parseInt(atual, 10);
+        u = parseInt(ultimo, 10);
+        sum = u / a;
+        clear_ultimo();
+        display_ultimo(sum);
+        clear_atual();
+    }
 }
